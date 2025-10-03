7974dfb8907e45040aa6b429942ff60ccc03d836
poravoz <116353344+poravoz@users.noreply.github.com>
2025-10-04 00:06:39 +0300

    some fix

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { LogOut } from "lucide-react";
+import { LogOut, MessageCircle } from "lucide-react";
 import { useAuthStore } from "../store/useAuthStore";
 import { useNavigate } from "react-router-dom";
 import "./NavBar.css";
@@ -15,17 +15,19 @@ export const NavBar = () => {
   return (
     <nav className="navbar_container">
       <div className="navbar_content">
-        <div className="navbar_logo">Comments App</div>
+        <div className="navbar_logo">
+          <MessageCircle className="logo-icon" />
+          <span>CommentBox</span>
+        </div>
         <div className="navbar_actions">
           {authUser && (
             <>
-            <span className="navbar_greeting">Hello, {authUser.name} !</span>
-            <button className="logout-button" onClick={handleLogout}>
-              <LogOut className="logout-icon" />
-              <span className="logout-text">Logout</span>
-            </button>
+              <span className="navbar_greeting">Hello, {authUser.name}!</span>
+              <button className="logout-button" onClick={handleLogout}>
+                <LogOut className="logout-icon" />
+                <span className="logout-text">Logout</span>
+              </button>
             </>
-
           )}
         </div>
       </div>
