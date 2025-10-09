c88cd6cf1f0a47bc072403a2c24650db1ee3b108
poravoz <116353344+poravoz@users.noreply.github.com>
2025-10-09 16:26:38 +0300

    port add to .env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+
+  const configService = app.get(ConfigService);
+  const clientOrigin = configService.get<string>('CLIENT_ORIGIN');
+
   app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,              
+    origin: clientOrigin,
+    credentials: true,
   });
-  await app.listen(5000);
+
+  const port = configService.get<number>('PORT');
+  if (!port) throw new Error('PORT is not defined');
+  await app.listen(port);
+  
 }
 bootstrap();
